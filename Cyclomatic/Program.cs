add92b9c8ac4efc1249d63bdbcef0210161861ba
selmaner <selmaner@A275-S16>
2025-11-03 17:46:17 +0100

    Ajoutez des fichiers projet.

---
+++ Cyclomatic/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Cyclomatic
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            int[] a = { 15, 28, 72, 95, 36, 80, 66, 45, 29, 19, 21, 12, 41, 37, 60, 11, 49, 94, 40, 62,
+                        85, 56, 70, 90, 50, 64, 16, 47, 87, 30, 76, 63, 57, 18, 91, 73, 68, 52, 78, 10,
+                       };
+
+            int it = BogoSort(a);
+            Console.WriteLine("Sorted array:");
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine($"{i}: {a[i]}");
+            }
+            Console.WriteLine($" iteration : {it}");
+        }
+
+        static int  BogoSort(int[] a)
+        {
+            int ct = 0;
+            int n = a.Length;
+            while (SortCheck(a) == false)
+            {
+                Shuffle(a);
+                ct++;
+                Console.WriteLine($" iteration : {ct} : {string.Join(", ", a)}");
+            }
+            return ct;
+        }
+
+        static bool SortCheck(int[] a)
+        {
+            bool rt = true; 
+            int n = a.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                if (a[i] > a[i + 1])
+                {
+                    rt = false;
+                    break;
+                }
+            }
+            return rt;
+        }
+
+        static void Shuffle(int[] a)
+        {
+            Random rnd = new Random();
+            int n = a.Length;
+
+            for (int i = 0; i < n; i++)
+            {
+                int r = rnd.Next(0, n);
+                int tmp = a[i];
+                a[i] = a[r];
+                a[r] = tmp;
+            }
+        }
+    }
+}
---
+++ Singleton/TestVendeur.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class TestVendeur
+    {
+
+        public static void Test()
+        {
+
+            Vendeur leVendeur = Vendeur.GetInstance();
+            leVendeur.nom = "Vendeur Auto";
+            leVendeur.adresse = "Paris";
+            leVendeur.email = "vendeur@vendeur.com";
+            affiche();
+        }
+  public static void affiche()
+        {
+            Vendeur leVendeur = Vendeur.GetInstance();
+            leVendeur.affiche();
+        }
+    }
+
+  }
+
